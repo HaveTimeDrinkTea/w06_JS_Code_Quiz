d672156d36cb0ff360239a0815b6a3fd32ed877c
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-01-24 10:58:59 +0000

    tidy up script.js

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -30,7 +30,6 @@ var timerCount;
 var quizActive = true;
 
 // set quiz related variables
-// var numQuestionMax = questionsArray.length;
 var numQuestionReq;
 var numQuestionCount;
 var chosenQuestion;
@@ -41,7 +40,7 @@ var questionSecAllowed; // 5 secs per question
 var userScoreCurr = 0;
 var penaltyCount = 0;
 var secondsDeducted = 0;
-var wrongAnsTimeDeduct = 2; 
+var wrongAnsTimeDeduct = 3; 
 
 // Array to hold user stats in the local storage
 var userStatsArray;
@@ -62,15 +61,14 @@ function initQuiz() {
     correctAnswer = "";
 }
 
-
-
+//- Create timer function
 
 function startTimer() {
     timerDivElQ.setAttribute("class", "timer");
     // Sets timer
     timer = setInterval(function() {
         timerCount--;
-        // console.log("timerCount",timerCount)
+
         timeCountDownSpanElQ.textContent = ": " + timerCount + "s left";
         if (timerCount >= 0) {
         // Tests if win condition is met
@@ -93,7 +91,7 @@ function startTimer() {
 
 
 
-// Start or restart the Quiz
+// Start or restart the Quiz with event listeners
 
 startButtonElQ.addEventListener("click", function(event) {
     initQuiz()
@@ -113,27 +111,24 @@ restartButtonElQ.addEventListener("click", function(event) {
 // Rendering the questions
 //- create a new array to hold all the questions first and then once the question is picked by getQuizQuestion() it will be removed from this array so that repeated questions will not be shown
 
-// Get the question first. Making sure there are no repeats
+// Step 1: Get the question first. Making sure there are no repeats
 function getQuizQuestion() {
-    console.log("questionsToChooseArray length:", questionsToChooseArray.length);
 
     let randomIndex = Math.floor(Math.random() * questionsToChooseArray.length);
 
-    console.log("randomIndex", randomIndex);
     chosenQuestion = [];
     chosenQuestion.push(questionsToChooseArray[randomIndex]);
-    console.log("chosenQuestion:",chosenQuestion);   
+
     // remove the chosen question from the array of questions
     if (questionsToChooseArray.length > 0) {
         questionsToChooseArray.splice(randomIndex,1);
     };
-    console.log("questionsToChooseArray:",questionsToChooseArray);
-    console.log("questions left:",questionsToChooseArray.length);
+
     numQuestionCount++;
 };
 //- end of function questionsToChooseArray()
 
-// render the question.
+// Step 2: render the question onto webpage
 function renderQuestion() {
 
     if (numQuestionCount < numQuestionReq) {
@@ -146,18 +141,15 @@ function renderQuestion() {
     timerPenaltyMsgSpanElQ.setAttribute("class","hide");
     endScreenDivElQ.setAttribute("class","hide");
     feedbackDivElQ.setAttribute("class","hide");
-
     questionsDivElQ.setAttribute("class","visible");
     questionChoicesDivElQ.addEventListener("click", activeQuiz);
     answerMsgDivElQ.innerHTML = "";
 
     userAnswer = "";
     correctAnswer = "";
-    console.log("Question:", numQuestionCount);
 
     questionTitleElQ.innerHTML = "Question " + numQuestionCount + ":<br><br>" + chosenQuestion[0].question;
     correctAnswer = chosenQuestion[0].answer;
-    console.log("Correct Answer:", correctAnswer);
 
     questionChoicesDivElQ.innerHTML = "<ol><li id='a'>"
         + chosenQuestion[0].option.a + "</li><li id='b'>" 
@@ -165,91 +157,74 @@ function renderQuestion() {
         + chosenQuestion[0].option.c + "</li><li id='d'>" 
         + chosenQuestion[0].option.d +"</li></ol>";
 
-        console.log("numQuestionCount:",numQuestionCount);
-        console.log("quizActive", quizActive);
     } else {
         endQuiz();
     };
 };
 
+//- end of function renderQuestion
 
-
-//- end function renderQuestion
+//- function to attach to the event listener on options of the MCQ so that user click can be registered and processed.
 
 function activeQuiz(event) {
     if(event.target && event.target.nodeName === "LI") {
-        console.log("I'm here!");
         userAnswer = event.target.id;
-        console.log("user answer:", userAnswer);
         checkAnswer();
         renderQuestion();
         return;
     }
 }
 
+// function to verify user's answer input
+// apply penalty if answer is wrong
 
 function checkAnswer() {
     console.log("checkAns== userAnswer is", userAnswer,"vs correctAnswer is", correctAnswer);
     if ( (userAnswer === correctAnswer)) {
         answerMsgDivElQ.innerHTML = "<i class='fa-solid fa-face-smile-halo'></i> Correct!!!!! 🙆🏻‍♂️";
-        console.log("correct!!!!! 🙆🏻‍♂️");
         userScoreCurr ++;
-        console.log("userScore:", userScoreCurr);
-} else {
-    answerMsgDivElQ.innerHTML = "<i class='fa-regular fa-face-woozy'></i> uh oh! Wrong answer!!!!! 🙅🏻‍♂️";
-    timerCount = timerCount -2; // time reduced by 2s if answer is wrong
-    penaltyCount ++;
-    secondsDeducted = penaltyCount * wrongAnsTimeDeduct;
+    } else {
+        answerMsgDivElQ.innerHTML = "<i class='fa-regular fa-face-woozy'></i> uh oh! Wrong answer!!!!! 🙅🏻‍♂️";
+        timerCount = timerCount - wrongAnsTimeDeduct; // time reduced by 3s if answer is wrong
+        penaltyCount ++;
+        secondsDeducted = penaltyCount * wrongAnsTimeDeduct;
 
-    timerPenaltyMsgSpanElQ.setAttribute("class","visible");
-    timerPenaltyMsgSpanElQ.innerHTML = "Penalty: " + penaltyCount + "<br>Total Seconds Deducted: " + secondsDeducted;
-
-    console.log("🙅🏻‍♂️ uh oh! Wrong answer!!!!! 🙅🏻‍♂️");
-    console.log("userScore:", userScoreCurr);
-};
+        timerPenaltyMsgSpanElQ.setAttribute("class","visible");
+        timerPenaltyMsgSpanElQ.innerHTML = "Penalty: " + penaltyCount + "<br>Total Seconds Deducted: " + secondsDeducted;
+    };
 }
 
+//- function for when quiz ends so that scores can be shown etc
 function endQuiz() {
-        // - numQuestionCount > numQuestionReq
-        //- end the quiz
-        console.log("finish!!!!");
-        quizActive = false;
+    
+    quizActive = false;
+    
+    // reveal the scores and messages 
+    highScoresDivElQ.setAttribute("class","scores");
+    endScreenDivElQ.setAttribute("class","visible");
 
-        console.log("quizActive", quizActive);
-        console.log("questionsToChooseArray length:",questionsToChooseArray.length);
-        highScoresDivElQ.setAttribute("class","scores");
-        endScreenDivElQ.setAttribute("class","visible");
-
-        //- set user score messages
-        console.log("userScore:",userScoreCurr);
-        console.log("numQuestionReq:",numQuestionReq);
-
-        if (userScoreCurr === numQuestionReq) {
-            userScoreMsgSpanElQ.innerHTML = "<i class='fa-solid fa-trophy-star'></i>Very Well done! You have a perfect score of " + userScoreCurr + " out of " + numQuestionReq +"!";
-        } else if ((userScoreCurr > 0) && (userScoreCurr < numQuestionReq)) {
-            userScoreMsgSpanElQ.innerHTML = "<i class='fa-solid fa-face-unamused'></i> Close but no cigar! Your score is " + userScoreCurr + ". <br> Aim for perfect score next time! <br> <i class='fa-solid fa-person-digging'></i> You can do it!";
-        } else {
-            userScoreMsgSpanElQ.innerHTML = "<i class='fa-solid fa-face-grimace'></i> Oh dear! Your score is " + userScoreCurr + ". <br> Please try harder! <i class='fa-solid fa-dumbbell'></i> You can make it!";
-        };
-        questionsDivElQ.setAttribute("class","hide");
-        questionChoicesDivElQ.removeEventListener("click", activeQuiz);
-        return;
-};
-
-
-
+    // hide the MCQ quiz area and remove listener on the mcq options.
+    questionsDivElQ.setAttribute("class","hide");
+    questionChoicesDivElQ.removeEventListener("click", activeQuiz);
+    
+    //- set user score messages
+    if (userScoreCurr === numQuestionReq) {
+        userScoreMsgSpanElQ.innerHTML = "<i class='fa-solid fa-trophy-star'></i>Very Well done! You have a perfect score of " + userScoreCurr + " out of " + numQuestionReq +"!";
+    } else if ((userScoreCurr > 0) && (userScoreCurr < numQuestionReq)) {
+        userScoreMsgSpanElQ.innerHTML = "<i class='fa-solid fa-face-unamused'></i> Close but no cigar! Your score is " + userScoreCurr + ". <br> Aim for perfect score next time! <br> <i class='fa-solid fa-person-digging'></i> You can do it!";
+    } else {
+        userScoreMsgSpanElQ.innerHTML = "<i class='fa-solid fa-face-grimace'></i> Oh dear! Your score is " + userScoreCurr + ". <br> Please try harder! <i class='fa-solid fa-dumbbell'></i> You can make it!";
+    };
+    
+    // add listener to the user submit button for high scores
     userSubmitBtnElQ.addEventListener("click", function(event) {
-        console.log("im' here!!!! after clicking submit");
         userInitials = initialsInputElQ.value;
-        console.log("userInitials:",userInitials)
         prepHighScore();
         return;
-        }
-    )
-;
+    });
 
-
-
+    return;
+};
 
 // updating High Scores in local storage
 
@@ -275,8 +250,7 @@ function prepHighScore() {
         sortUserStats();
         prevMinScore = userStatsStored[userStatsStored.length - 1].userScore;
     };
-    console.log("here!:", userStatsStored)
-    console.log("prevMinScore:", prevMinScore);
+
 // Updates win count on screen and sets win count to client storage 
 
     if (userStatsStored.length < maxHighScoresUsers) {
@@ -325,6 +299,6 @@ function prepHighScore() {
 }
 
 
-
+// initialize the page on load
 initQuiz();
 
